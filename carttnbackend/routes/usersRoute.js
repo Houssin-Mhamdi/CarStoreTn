9e5d016fb91e3b4e2e1d1232200b599a9111713a
houssinCarnelian <houssin.carnelian@gmail.com>
2023-03-14 16:17:03 +0100

    add validation the id invalid id if id not an objectID

--- carttnbackend/routes/usersRoute.js
+++ carttnbackend/routes/usersRoute.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const { getAllUsersCtrl, getUserProfileCtrl } = require('../controllers/usersController')
+const validateObjectId = require('../middlewares/validateObjectId')
 
 
 router.get('/profile', getAllUsersCtrl)
-router.get('/profile/:id', getUserProfileCtrl)
-//router.route('profile/:id').get(getUserProfileCtrl)
+//router.get('/profile/:id', getUserProfileCtrl)
+router.route('/profile/:id').get(validateObjectId,getUserProfileCtrl)
 module.exports = router
